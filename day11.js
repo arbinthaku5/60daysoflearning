e731ba641dcec19fa883de89766aa1c7bcd5022a
Thaku Arbin <arbinthaku@gmail.com>
2025-06-11 22:30:40 +0545

    2nd edit

--- day11.js
+++ day11.js
@@ -1,10 +1,18 @@
 const fs = require('fs');
 
+//write to file
 fs.writeFile('example.txt', 'This is some sample text.', (err) => {
   if (err) throw err;
+  console.log('File written successfully');
 
+//read a file
   fs.readFile('example.txt', 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
   });
+});
+
+fs.appendFile('output.txt', '\nMore content here...', (err) => {
+  if (err) throw err;
+  console.log('Appended content');
 });
